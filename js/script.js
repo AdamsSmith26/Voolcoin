bee7b4175f3cc590e16190bd33e9fd99404ae7a9
Adam <adamssmith2003@gmail.com>
2023-06-23 22:25:39 +0400

    second

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// burger menu
+document.getElementById("burger").addEventListener("click", function () {
+    document.getElementsByClassName("mynav")[0].classList.toggle("open");
+    document.getElementsByClassName("nav-btn")[0].classList.toggle("myopacity");
+});
